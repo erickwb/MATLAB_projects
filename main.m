6ba556cdcd714cdae2de1f89740300d15a0e6fbc
Erick Correia Silva <silvaerick331@gmail.com>
2022-01-19 21:40:55 -0300

    implementação da convoluçao de sinal

--- main.m
+++ main.m
@@ -2,20 +2,20 @@ close all
 clear
 clc
 
-% %Teste função Decompoem_sinal
+%Teste função Decompoem_sinal
 % n1 = input('Entre com os valores de n'); %pegando o intervalo n
-% x = input('Entre com a seguencia x'); %montando o sinal x
+% x = input('Entre com a sequencia x'); %montando o sinal x
 % 
 % 
 % Decompoem_sinal(x, n1);
 
-%%Teste função Convolução
-% n_x = input('Entre com os valores de n_x');
-% n_h = input('Entre com os valores de n_h');
-% x = input('Entre com os valores de x');
-% h = input('Entre com os valores de h');
-% 
-% Convolucao(x,n_x,h, n_h);
+%Teste função Convolução
+n_x = input('Entre com os valores de n_x');
+n_h = input('Entre com os valores de n_h');
+x = input('Entre com os valores de x');
+h = input('Entre com os valores de h');
+
+Convolucao(x,n_x,h, n_h);
 
 
 
